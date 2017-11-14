1592c7ecd166099e926c386933a0229323eaf4f5
ssingularity <1755405704@qq.com>
2017-11-14 10:25:59 +0800

    Add a ful_titlt helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,11 +5,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 		get root_url
 		assert_response :success
 	end
-	
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
